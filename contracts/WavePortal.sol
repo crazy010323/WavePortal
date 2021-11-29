fe2d01640d87717c31684cd6055096bd2c51504d
Robert M. Carden <crazy010323@gmail.com>
2021-11-29 13:56:05 -0500

    messaging functionality added

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,19 +5,19 @@ import "hardhat/console.sol";
 
 contract WavePortal {
 
-    mapping (address => uint)   waves;
+    mapping (address => string[])   waves;
 
     constructor() {
         console.log("I'm a contract and i'm smart");
     }
 
-    function wave() public {
-        waves[msg.sender] ++;
-        console.log("%s has waved!!!", msg.sender);
+    function wave(string memory _message) public {
+        waves[msg.sender].push(_message);
+        console.log("%s has waved:", msg.sender, _message);
     }
 
-    function getWaves(address waver) view public returns (uint) {
-        console.log("We have %d wave(s) from %s!", waves[waver], waver);
+    function getWaves(address waver) view public returns (string[] memory) {
+        console.log("We have %d wave(s) from %s!", waves[waver].length, waver);
         return waves[waver];
     }
 
